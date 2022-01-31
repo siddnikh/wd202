c50110f7059a4e050d8ebf7a041db4f9dcad89a2
siddnikh <mailtosiddnikh@gmail.com>
2022-01-31 14:51:56 +0530

    Added user foreign key

--- level5/tasks/tests.py
+++ level5/tasks/tests.py
@@ -1,25 +1,29 @@
 from django.test import TestCase
 
+from tasks.models import Task
+
 # Create your tests here.
 
 
 class TestTaskManager(TestCase):
-    def test_a_add_task(self):
+    def test_add_task(self):
         """Test Adding Tasks"""
         task_name = "Test1"
         response = self.client.get("/add-task/", {"task": task_name})
         self.assertEqual(response.status_code, 302)
 
-    def test_a_delete_task(self):
+    def test_delete_task(self):
         """Test Deleting Tasks"""
         task_name = "Test2"
         self.client.get("/add-task/", {"task": task_name})
-        response = self.client.get("/delete-task/1/")
+        task_object = Task.objects.filter(title=task_name).first()
+        self.assertIsNotNone(task_object)
+        response = self.client.get(f"/delete-task/{task_object.id}/")
         self.assertEqual(response.status_code, 302)
 
     def test_a_view_task(self):
         """Test Viewing Tasks"""
-        task_name = "Luke Skywalker"
+        task_name = "learn to use the force"
         self.client.get("/add-task/", {"task": task_name})
         response = self.client.get("/tasks/")
         self.assertEqual(response.status_code, 200)
@@ -27,13 +31,14 @@ class TestTaskManager(TestCase):
 
     def test_b_view_task(self):
         """Complete a given task and ensure its in the completed view"""
-        task_name = "Darth Vader"
+        task_name = "learn the ways of the force"
         self.client.get("/add-task/", {"task": task_name})
         response = self.client.get("/completed_tasks/")
         self.assertEqual(response.status_code, 200)
         self.assertNotIn(task_name, response.content.decode())
-        t = self.client.get("/complete_task/3/")
-        print(t)
+        task_object = Task.objects.filter(title=task_name).first()
+        self.assertIsNotNone(task_object)
+        t = self.client.get(f"/complete_task/{task_object.id}/")
         response = self.client.get("/tasks/")
         self.assertEqual(response.status_code, 200)
         self.assertNotIn(task_name, response.content.decode())
