432670ac94ef5ecd5f7df747565c4b540c52936f
siddnikh <mailtosiddnikh@gmail.com>
2022-01-31 14:32:35 +0530

    Shifted to Database use

--- level5/tasks/models.py
+++ level5/tasks/models.py
@@ -4,3 +4,12 @@ class Task(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     completed = models.BooleanField(default = False)
+    created_date = models.DateTimeField(auto_now = True)
+    deleted = models.BooleanField(default = False)
+
+    def delete(self):
+        self.deleted = True
+        self.save()
+
+    def __str__(self):
+        return self.title
--- level5/tasks/views.py
+++ level5/tasks/views.py
@@ -1,36 +1,44 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-
-tasks = []
-tasks_done = []
+from .models import Task
 
 def tasks_view(request):
-    return render(request, "tasks/tasks.html", {
-        "avaialable": (len(tasks_done) > 0),
-        "tasks": tasks
+    search_term = request.GET.get("search")
+    if search_term:
+        tasks = Task.objects.filter(title__icontains = search_term)
+    else:
+        tasks = Task.objects.filter(completed = False, deleted = False)
+    tasks_done = Task.objects.filter(completed = True, deleted = False)
+    return render(request, 'tasks/tasks.html', {
+        "tasks": tasks,
+        "available": (len(tasks_done) > 0)
     })
 
 def add_task(request):
     task = request.GET.get("task")
-    tasks.append(task)
+    Task(title = task).save()
     return redirect('/tasks')
 
 def delete_task(request, index): 
-    del tasks[index - 1]
+    Task.objects.get(id = index).delete() #using overloaded function
     return redirect('/tasks')
 
 def complete_task(request, index):
-    tasks_done.append(tasks[index - 1])
-    del tasks[index - 1]
+    t = Task.objects.get(id = index)
+    t.completed = True
+    t.save()
     return redirect('/tasks')
 
 def completed_tasks(request):
+    tasks = Task.objects.all().filter(completed = True)
     return render(request, 'tasks/completed.html', {
-        "avaialable": (len(tasks_done) > 0),
-        "completed": tasks_done,
+        "avaialable": (len(tasks) > 0),
+        "completed": tasks,
     })
 
 def all_tasks(request):
+    tasks = Task.objects.filter(completed = False, deleted = False)
+    tasks_done = Task.objects.filter(completed = True, deleted = False)
     return render(request, "tasks/all_tasks.html", {
         "completed" : tasks_done,
         "pending": tasks
