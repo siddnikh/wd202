51d960447a6e79059cd1abdf2dea77c4abd52c5a
siddnikh <mailtosiddnikh@gmail.com>
2022-02-22 19:48:29 +0530

    Removed unneccessary code

--- level6/tasks/models.py
+++ level6/tasks/models.py
@@ -13,11 +13,7 @@ class Task(models.Model):
     def delete(self):
         self.deleted = True
         self.save()
-    
-    def increase_priority(self):
-        self.priority += 1
-        self.save()
-    
+
     def pretty_date(self):
         return self.created_date.strftime("%a %d %b")
 
